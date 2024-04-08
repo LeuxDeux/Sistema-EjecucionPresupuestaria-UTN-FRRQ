340536d7de528f19b4a01fe2247468be6f208cae
LeuxDeux <yoyoyzacarias@gmail.com>
2024-04-08 00:28:18 -0300

    modificación de estilos css leve, instalación de express-session y sistema de registro completado

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
-const mysql = require('mysql2');
 
 const app = express();
 //motor plantillas
 app.set('view engine', 'ejs');
 //carpeta public static
 app.use(express.static('public'));
+app.use('/resources', express.static(__dirname + '/public'));
 //urlencoded
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); //permite recibir json en las rutas
@@ -15,6 +15,14 @@ app.use(express.json()); //permite recibir json en las rutas
 dotenv.config({path: './env/.env'});
 
 const connection = require('./database/db');
+const bcryptjs = require('bcryptjs'); 
+const session = require('express-session');     //manejo de sesiones
+app.use(session({
+    secret:'secret',       //clave secreta para cifrar las cookies
+    resave: true,            //guarda la sesion aunque no haya cambios
+    saveUninitialized:true   //guarda la cookie aunque no haya sido inicializada previamente
+}));
+const mysql = require('mysql2');
 //cookies
 // app.use(cookieParser());
 // //eliminar cache
@@ -43,6 +51,43 @@ app.get('/registro', (req, res)=>{
             throw error;
         }else{
             res.render('registro', {results:results});
+        }
+    });
+});
+app.post('/register', async (req, res) => {
+    const usuario = req.body.usuario;
+    const nombres = req.body.nombres;
+    const secretaria = req.body.secretaria;
+    const contraseña = req.body.contraseña;
+    const email = req.body.email;
+    let contraseñaHash = await bcryptjs.hash(contraseña, 8);
+    connection.query('INSERT INTO usuarios SET ?', {
+        nombres: nombres,
+        email: email,
+        password: contraseñaHash,
+        secretaria_id: secretaria,
+        usuario: usuario
+    }, async (error, results) => {
+        if (error) {
+            throw error;
+        } else {
+            // Renderizar la plantilla 'registro.ejs' pasando 'results' junto con las variables relacionadas con el mensaje de alerta
+            connection.query('SELECT * FROM secretarias', (error, resultsSecretarias) => {
+                if (error) {
+                    throw error;
+                } else {
+                    res.render('registro', {
+                        results: resultsSecretarias, // Pasar 'resultsSecretarias' que contiene las secretarías
+                        alert: true,
+                        alertTitle: "Registro",
+                        alertMessage: "¡Registro Exitoso!",
+                        alertIcon: 'success',
+                        showConfirmButton: false,
+                        timer: 1500,
+                        ruta: ''
+                    });
+                }
+            });
         }
     });
 });
